11196699745166cd414f7dbc298f4736688ee20e
michael smith and neil larion <neil.larion@gmail.com>
2016-04-06 16:19:57 -0700

    Component that didnt work

--- app/components/display-rating.js
+++ app/components/display-rating.js
@@ -3,5 +3,15 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   model() {
     return this.store.findAll('rating');
+    // return this.store.findAll('product');
   },
+  averageRating: Ember.computed('rating.stars','rating.product',function(){
+    var myRating = this.get('rating');
+    console.log(myRating);
+    var ratingSum = 0;
+    for (var i = 0; i < myRating.stars.length; i++) {
+      ratingSum += parseInt(myRating.stars[i].get('stars'));
+    }
+    //return ratingSum/rating.stars.length
+  })
 });
