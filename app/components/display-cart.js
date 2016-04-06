1b3b8c667a398b5192727b02ccbe687df7103278
michael smith and neil larion <neil.larion@gmail.com>
2016-04-06 09:20:15 -0700

    add a way to remove items from our cart.

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -2,4 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   shoppingCart: Ember.inject.service(),
+
+  actions: {
+    removeItem(item) {
+      this.get('shoppingCart').remove(item);
+    }
+  }
 });
